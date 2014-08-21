876a8b156be2954cb74339eb9811f8a31d417aa0
root <root@kahlua-1.cloud2class.com>
2014-08-21 07:26:15 +0000

    Second Commit

--- js/jquery.FloatLabel.js
+++ js/jquery.FloatLabel.js
@@ -13,20 +13,26 @@
 			var element = $(this),
 				label = element.find('label'),
 				input = element.find('textarea, input');
-
-      if( input.val() == '' ) {
+					input.val( label.text() );
+      if(input.val() == '') {
         input.val( label.text() );
       } else {
         element.addClass( settings.populatedClass );
       }
-
 			input.on( 'focus', function(){
 				element.addClass( settings.focusedClass );
-				
-				if( input.val() === label.text() ){
+                                if( input.val() === label.text() ){
 					input.val('');
+                                if(element.find('#input-holder').hasClass('input-group')){
+                                  element.find('#input-holder').removeClass('input-group');
+                                  element.find('.input-group-addon').hide();
+                                }
 				} else {
 					element.addClass( settings.populatedClass );
+                                if(element.find('#input-holder').hasClass('input-group')){
+                                  element.find('#input-holder').removeClass('input-group');
+                                  element.find('.input-group-addon').hide();
+                                }
 				}
 
 			});
@@ -37,8 +43,12 @@
 				if( !input.val() ){
 					input.val( label.text() );
 					element.removeClass( settings.populatedClass );
+					element.find('#input-holder').attr('class', 'input-group');
+					element.find('.input-group-addon').css('display', 'table-cell');
+				} else {
+                                 element.find('#input-holder').removeClass('input-group');
+                                 element.find('.input-group-addon').hide();
 				}
-
 			});
 
 			input.on( 'keyup', function(){
